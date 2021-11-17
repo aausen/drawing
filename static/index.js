2a5c438cf780e982431e4ebcaaf8db0a2e5a082e
Annie Ausen <ausen.annie@gmail.com>
2021-11-17 16:09:33 -0600

    finish basic canvas in js

--- static/index.js
+++ static/index.js
@@ -1,16 +1,56 @@
 'use strict';
 
 function draw(){
-    var canvas = document.getElementById("myCanvas");
+    const canvas = document.getElementById("paint");
     if (canvas.getContext) {
-        var ctx = canvas.getContext("2d");
-        ctx.fillStyle = 'rbg(200, 0, 0)';
-        ctx.fillRect(10, 10, 50, 50)
+        const ctx = canvas.getContext("2d");
+        
+        const myCanvas = document.getElementById("myCanvas");
+        let myCanvas_style = getComputedStyle(myCanvas);
+        canvas.width = 500;
+        canvas.height = 250;
 
-        ctx.fillStyle = 'rbga(0, 0, 200. 0.5)';
-        ctx.fillRect(30, 30, 50, 50)
+        let mouse = {x:0, y:0};
+
+    // refactor into another function for mouse recapture //
+        canvas.addEventListener('mousemove', function(e){
+            mouse.x = e.pageX - this.offsetLeft;
+            mouse.y = e.pageY - this.offsetTop;
+        }, false);
+    
+    // refactor for new fuction for drawing
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';
+
+        ctx.strokeStyle = 'red';
+
+        function getColor(color){
+            strokeStyle = color;
+        }
+
+        function getSize(size){
+            ctx.lineWidth = size;
+        }
+
+        canvas.addEventListener('mousedown', function(e) {
+            ctx.beginPath();
+            ctx.moveTo(mouse.x, mouse.y);
+
+            canvas.addEventListener('mousemove', onPaint, false);
+        }, false);
+
+        canvas.addEventListener('mouseup', function() {
+            canvas.removeEventListener('mousemove', onPaint, false);
+        }, false);
+
+        let onPaint = function() {
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
+        };
+
     } else {
         console.log("it's not working!")
+        // replace with code for browsers that don't suport canvas 
     }
 }
 
